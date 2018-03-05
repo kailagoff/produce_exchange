3344077febd61bfeae57f8cce72461f6475d4a5f
Anna Marie Cooper and Kaila Goff <anna@>
2018-03-05 11:41:00 -0800

    add login form

--- app.rb
+++ app.rb
@@ -23,8 +23,17 @@ get('/available') do
   erb:available
 end
 
+get('/createaccount') do
+  erb:createaccount
+end
+
 post('/produce') do
   produce = Produce.create({:produce_type=> params['produce_type'], :description => params['description'], :trade => params['trade'], :id => nil})
   @produce = Produce.all
   erb:available
+end
+
+post('/account') do
+  user = User.create({:name=> params['name'], :password => params['password'], :quadrant => params['quadrant'], :id => nil})
+  erb:index
 end
