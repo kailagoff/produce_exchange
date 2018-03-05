4207e0ec70b87e7b63eeafa7d47b0e34a6839e94
Anna Marie Cooper and Kaila Goff <anna@>
2018-03-05 13:03:54 -0800

    adjust merge conflicts

--- app.rb
+++ app.rb
@@ -9,6 +9,33 @@ require("./lib/event")
 require("pg")
 require("pry")
 
+get('/') do
+  erb:index
+end
+
+get('/produce') do
+  erb:produce_form
+end
+
+get('/available') do
+  @produce = Produce.all
+  erb:available
+end
+
+get('/createaccount') do
+  erb:createaccount
+end
+
+post('/produce') do
+  produce = Produce.create({:produce_type=> params['produce_type'], :description => params['description'], :trade => params['trade'], :id => nil})
+  @produce = Produce.all
+  erb:available
+end
+
+post('/account') do
+  user = User.create({:name=> params['name'], :password => params['password'], :quadrant => params['quadrant'], :id => nil})
+  erb:index
+end
 
 get('/') do
   erb :home
